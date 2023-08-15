1e0ebbba8f23e783f39de3eb76e8f0e63ded7f2e
Youssouf Sergma <sil3nthilldz@gmail.com>
2023-08-15 13:07:53 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const parag = document.getElementById("text");
+    parag.textContent= "This is really cool!"
+  });
